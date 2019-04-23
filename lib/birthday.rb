bf2579e5a714ac6f10fb6e45706c310e01a0b863
Sarah Anderson <saranderson13@gmail.com>
2019-04-23 03:52:19 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,7 +10,7 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |child, age|
-    age 
+    puts age < 13 ? "Happy Birthday #{child}! You are now #{age} years old!" : "You are too old for this."
   end
 end
 
